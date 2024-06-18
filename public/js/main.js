c5f3eddcf5782cf5f7470fa0841abeb9c1d6ffea
AlexadnerEhr <alexander.ehrlich@icloud.com>
2024-06-18 20:37:22 +0200

    Initial commit

--- public/js/main.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Seite geladen und bereit.");
-});
--- src/config/config.php
+++ src/config/config.php
@@ -6,7 +6,7 @@ define('HEADER_PATH', TEMPLATE_PATH . 'header.php');
 define('FOOTER_PATH', TEMPLATE_PATH . 'footer.php');
 
 // Pfade zu CSS-Dateien
-define('CSS_PATH', 'css/styles/');
+define('CSS_PATH', 'css/');
 define('GLOBAL_CSS', CSS_PATH . 'global.css');
 define('HEADER_CSS', CSS_PATH . 'header.css');
 define('FOOTER_CSS', CSS_PATH . 'footer.css');
@@ -21,13 +21,13 @@ define('IMAGE_PATH', 'images/');
 define('INSTAICON', IMAGE_PATH . 'istaicon.png');
 define('GITHUBICON', IMAGE_PATH . 'githubicon.png');
 define('WILLKOMMEN', IMAGE_PATH . 'willkommenlogo.png');
-define('PROFILE_PICTURE', IMAGE_PATH . 'profilepicture2.jpg');
+define('PROFILE_PICTURE', IMAGE_PATH . 'profilepicture.WebP');
 
 
 
 // Pfade zu Videos
 define('VID_PATH', 'video/');
-define('TEST_VID', VID_PATH . 'alexVorstellung.mp4');
+define('TEST_VID', VID_PATH . 'infoaevid.mp4');
 
 
 // Funktionspfade
